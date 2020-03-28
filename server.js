b7ee41d88bc6d8c602a7d12848ac46c80c1a907f
Finley <finleyjchen@gmail.com>
2020-03-28 15:52:26 -0700

    added dotenv

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
 
+require('dotenv').load();
 const dev = process.env.NODE_ENV !== 'production';
 const next = require('next');
 const pathMatch = require('path-match');
